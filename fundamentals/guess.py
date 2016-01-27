67aad83b5f4d215fac9b51b1c76b360899decbb3
Lolo Fonz Tres <barrenec@hotmail.com>
2016-01-27 20:57:33 +0100

    puzzle game
    6 kyu, yeah

---
+++ fundamentals/guess.py
@@ -0,0 +1,28 @@
+'''
+
+The test fixture I use for this kata is pre-populated.
+
+It will compare your guess to a random number generated in Python by:
+
+randint(1,100)
+
+
+You can pass by relying on luck or skill but try not to rely on luck.
+
+"The power to define the situation is the ultimate power." - Jerry Rubin
+
+Good luck!
+
+
+'''
+
+
+from random import randint
+
+def new_randint(x,y):
+	return 69
+
+randint = new_randint	
+
+guess = randint(1,100) 
+print guess 
